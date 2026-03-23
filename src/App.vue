033ad7898b253cf6bdafe73f6b1b5e0619202fba
PedroKeita <pedrolucas300805@gmail.com>
2026-03-23 08:45:44 -0300

    feature: update App.vue and create home page layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
+<template>
+  <div class="min-h-screen bg-[#0a0c10]">
+    <RouterView />
+  </div>
+</template>
+
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
 </script>
-
-<template>
-  <HelloWorld />
-</template>
