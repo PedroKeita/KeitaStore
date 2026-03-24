e2686acce2ffd19dd2f5ba92456053a7fd489432
PedroKeita <pedrolucas300805@gmail.com>
2026-03-24 20:18:09 -0300

    fix: fix type port for number

--- api/src/server.ts
+++ api/src/server.ts
@@ -8,7 +8,7 @@ import userRoutes from './routes/user.js'
 dotenv.config()
 
 const app = express()
-const PORT = process.env.PORT || 3000
+const PORT = Number(process.env.PORT) || 3000
 
 
 
@@ -26,37 +26,8 @@ app.use(passport.initialize())
 app.use('/auth', authRoutes)
 app.use('/user', userRoutes)
 
-import express from 'express'
-import cors from 'cors'
-import dotenv from 'dotenv'
-import passport from 'passport'
-import authRoutes from './routes/auth.js'
-import userRoutes from './routes/user.js'
-
-dotenv.config()
-
-const app = express()
-const PORT = process.env.PORT || 3000
-
-
-
-app.use(cors({
-  origin: [
-    'http://localhost:5173',
-    'https://keita-store.vercel.app' 
-  ],
-  credentials: true
-}))
-
-app.use(express.json())
-app.use(passport.initialize())
-
-app.use('/auth', authRoutes)
-app.use('/user', userRoutes)
-
-app.listen(PORT, () => {
-  console.log(`API rodando em http://localhost:${PORT}`)
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`API rodando na porta ${PORT}`)
 })
-
 
 export default app
