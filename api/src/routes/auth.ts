83f2642690cfbb259a92e5b0d3f50df395da4c46
PedroKeita <pedrolucas300805@gmail.com>
2026-03-27 09:56:48 -0300

    fix: fix callback

--- api/src/routes/auth.ts
+++ api/src/routes/auth.ts
@@ -11,7 +11,7 @@ passport.use(
         {
             clientID: process.env.GOOGLE_CLIENT_ID!,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
-            callbackURL: `${process.env.API_URL}/auth/google/callback`,
+            callbackURL: `https://keitastore-api.onrender.com/auth/google/callback`,
         },
 
         async (_accessToken, refreshToken, profile, done) => {
